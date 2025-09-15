73e2bbe02f14628b5f29d876767313cfc05465fe
Alejotamayo28 <alejandropucca0@gmail.com>
2025-09-15 10:48:35 -0500

    environment variables added and improved

--- src/auth/admin.auth.ts
+++ src/auth/admin.auth.ts
@@ -1,5 +1,6 @@
 const validateId = new Set<number>();
 
+//Validated users Telegrams users
 export function checkValidateId(id: number, envAdminId: number): boolean {
   validateId.add(Number(envAdminId))
   return validateId.has(id);
--- src/services/ec2/ec2-configs/test.config.ts
+++ src/services/ec2/ec2-configs/test.config.ts
@@ -1,14 +1,18 @@
 import { RunInstancesCommandInput } from "@aws-sdk/client-ec2";
+import dotenv from "dotenv"
 
+dotenv.config()
+
+// Instancia basica / barata de AWS EC2
 export function testEc2Config(): RunInstancesCommandInput {
   return {
-    ImageId: 'ami-04b4f1a9cf54c11d0',
+    ImageId: process.env.AWS_EC2_IMAGE_ID,
     InstanceType: 't2.micro',
     MinCount: 1,
     MaxCount: 1,
-    SubnetId: 'subnet-0dab0123269f15b16',
-    SecurityGroupIds: ['sg-0f444c6a7432cb924'],
-    KeyName: 'DockerBot-Key',
+    SubnetId: process.env.AWS_EC2_SUBNET_ID,
+    SecurityGroupIds: [process.env.AWS_EC2_SECURITY_GROUP!],
+    KeyName: process.env.AWS_EC2_KEY_NAME,
     TagSpecifications: [
       {
         ResourceType: "instance",
